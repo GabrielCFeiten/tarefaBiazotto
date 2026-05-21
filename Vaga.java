d280d224dced6c6418d4f7adb4a0d09d8c96bf0a
Gabriel Campos Feiten <gabrielcamposfeiten@gmail.com>
2026-05-21 15:08:44 -0300

    Biazotto

---
+++ Vaga.java
@@ -0,0 +1,32 @@
+package br.edu.refactoring.estacionamento;
+
+public class Vaga {
+
+    private String codigo;
+    private String setor;
+    private boolean coberta;
+    private boolean ocupada;
+
+    public Vaga(String codigo, String setor, boolean coberta, boolean ocupada) {
+        this.codigo = codigo;
+        this.setor = setor;
+        this.coberta = coberta;
+        this.ocupada = ocupada;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public boolean isCoberta() {
+        return coberta;
+    }
+
+    public boolean isOcupada() {
+        return ocupada;
+    }
+}
